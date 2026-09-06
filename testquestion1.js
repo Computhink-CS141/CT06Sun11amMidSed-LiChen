15baa1b1f13d85aebc78849c75ccd309defba207
Computhink-CS141 <cs141@computhink.com.sg>
2026-09-06 04:04:58 +0000

    Sep 6, 2026, 4:04 AM

--- testquestion1.js
+++ testquestion1.js
@@ -11,7 +11,7 @@ function draw(){
     stroke("red");
     strokeWeight(10);
     circle(70, 125, 50);
-    triangle(300, 200, 290, 210, 350, 250);
+    triangle(300, 200, 250, 250, 350, 250);
     rect(200, 100, 100, 100);
 }
 
