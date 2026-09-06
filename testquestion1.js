e4cb0767a902e4a2194b1d7cd73aac307c6a7485
Computhink-CS141 <cs141@computhink.com.sg>
2026-09-06 03:42:19 +0000

    Sep 6, 2026, 3:42 AM

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,20 @@
+let xpos;
+let ypos;
+
+function setup () {
+    createCanvas(600, 400);
+    background("255")
+}
+
+function draw(){
+    background("black")
+    fill("black");
+    stroke("red");
+    strokeWeight(10);
+    circle(xpos, ypos, 50);
+    triangle(175, 100, 150, 120, 75, 50);
+    // rectangle(400, 300, 100, 100);
+    xpos = xpos + 55;
+    ypos = ypos + 55;
+}
 
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,18 @@
+let xpos=50;
+let ypos=50;
+let colourMe=0;
 
+function setup(){
+    createCanvas(600, 600);
+    background("grey");
+}
+
+function draw() {
+    
+    for (let num = 1; num <= 11; num = num + 1) {
+        fill(colourMe);
+        circle(xpos, ypos, 50);
+        xpos = xpos + 55
+        ypos = ypos + 55
+    }
+}
