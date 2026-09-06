b198982ec23a73a772c0ba9623355f4b48f59f19
Computhink-CS141 <cs141@computhink.com.sg>
2026-09-06 03:46:59 +0000

    Sep 6, 2026, 3:46 AM

--- testquestion2.js
+++ testquestion2.js
@@ -5,7 +5,7 @@ let colourMe=0;
 function setup(){
     createCanvas(600, 600);
     background("grey");
-    for (let num = 1; num <= 11; num = num + 1) {
+    for (let num = 1; num <= 10; num = num + 1) {
         fill(colourMe);
         circle(xpos, ypos, 50);
         xpos = xpos + 50
